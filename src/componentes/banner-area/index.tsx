4d9120cd40a0bd15d61c812da74a9707e7991561
Augusto Moraes <moraezaugusto@gmail.com>
2023-08-08 18:41:49 -0300

    path deploy/ project added: GameDev

--- src/componentes/banner-area/index.tsx
+++ src/componentes/banner-area/index.tsx
@@ -12,12 +12,12 @@ export const BannerArea = () => {
                 <a href="#projetos" ><BsFillBoxFill/>Projetos</a>
                 <a href="https://www.linkedin.com/in/moraesaugusto/" target="_blank"><BsLinkedin/>Linkedin</a>
                 <a href="https://github.com/AugustoMoraez" target="_blank"><BsGithub/>GitHub</a>
-                <a href="https://drive.google.com/file/d/146bPSzeGi5T8zA9lFGk9dTwwrFOUmVlS/view?usp=drive_link" target="_blank"><BsDownload/>Curriculo</a>
+                <a href="https://drive.google.com/file/d/1JOi27KXp1c1CeWvVcGPi88mv-lDvW7m_/view?usp=drive_link" target="_blank"><BsDownload/>Curriculo</a>
             </div>
             <div className="banner-titulo">
                 <p>Me chamo Augusto e sou</p>
                 <div className="titulo">
-                    <p>REACT.JS</p>
+                    <p>FRONTEND</p>
                     <p>DEVELOPER</p>
                 </div>
                 <p>1 ano de experiência</p>
--- src/data/projetos.ts
+++ src/data/projetos.ts
@@ -1,19 +1,19 @@
 import projeto1 from "../assets/images/pizzaKing.png"
-import projeto2 from "../assets/images/cineDev.png"
+import projeto2 from "../assets/images/gamedev.png"
 
 export const data = [
     {
         titulo:"Pizzaria King",
-        sobre:`Desenvolvido com a poderosa tecnologia React.js, fico orgulhoso em oferecer a você uma experiência de compra de pizza online excepcional. Este site foi cuidadosamente projetado para facilitar a sua jornada gastronômica, permitindo que você adicione e remova itens do carrinho com apenas alguns cliques, além de finalizar sua compra de forma simples e segura. Graças à tecnologia React.js, o este site é altamente responsivo e oferece uma experiência de usuário fluida em diferentes dispositivos, seja você um cliente navegando no computador, tablet ou smartphone. Me comprometi em fornecer uma interface agradável e amigável, tornando a sua experiência de compra de pizza online o mais conveniente possível.`,
+        sobre:`Interface grafica de um sistema de delivery de um pizzaria, onde é possivel adicionar e remover itens no carrinho, podendo visualizar o valor da compra em tempo real. Possui um banner na pagina principal com paginas que ainda serão adicionadas no projeto.`,
         tec:"React.js, Typescript, ContextAPI, Styled Components, React-Router-Dom, React Icons, UUID.",
         img:projeto1,
         url:"https://hilarious-strudel-4649eb.netlify.app/"
     },
     {
-        titulo:"Cinema Dev",
-        sobre:"Fico entusiasmado em apresentar a você uma plataforma de cinema online inovadora. Este site foi criado para proporcionar uma experiência completa e personalizada, permitindo que você descubra os filmes em exibição, escolha a poltrona e horário de acordo com a disponibilidade e compre um delicioso combo de comida para aproveitar durante a sessão. Com uma interface moderna e amigável, você pode explorar nossa extensa lista de filmes em exibição e verificar os horários disponíveis para cada um deles. Graças à tecnologia React.js, nosso site oferece uma experiência de usuário fluida e responsiva, adaptando-se perfeitamente a diferentes dispositivos. ",
-        tec:"React.js, Typescript, Redux.js, Styled Components, React-Router-Dom, React Icons.",
+        titulo:"Game Dev",
+        sobre:"Sistema no qual é possivel realizar um login atraves do firebase authentication, adicionar e remover itens a sua lista de favoritos, criar avaliações, acessar pagina de jogos e filtrar e pesquisar os jogos por nome e gênero.",
+        tec:"React.js, Typescript, Redux.js, Styled Components, React-Router-Dom, React Firebase Hooks.",
         img:projeto2,
-        url:"https://cine-o26q0qad0-augustomoraez.vercel.app/",
+        url:"https://gamedev-gamma.vercel.app/",
     }
 ]
